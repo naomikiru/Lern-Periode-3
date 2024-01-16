e9a5866d1bcb3aaa5653d35929ef4cce77a2e619
naomikiru <142885893+naomikiru@users.noreply.github.com>
2024-01-16 11:26:57 +0100

    Add files via upload

---
+++ Rechner.cs
@@ -0,0 +1,27 @@
+﻿namespace Lern_Periode_3
+{
+    internal class Rechner
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            int zahl1 = random.Next(1, 101);
+            int zahl2 = random.Next(1, 101);
+
+            bool Addition = random.Next(0, 2) == 0;
+
+            if (Addition)
+            {
+                Console.Write($"Was ist {zahl1} + {zahl2}? ");
+            }
+            else
+            {
+                Console.Write($"Was ist {zahl1} - {zahl2}? ");
+            }
+
+        }
+    }
+}
+
+ 
