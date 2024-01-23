19b9c0a5e050337819e3497e34ba93b575711abe
naomikiru <142885893+naomikiru@users.noreply.github.com>
2024-01-23 11:21:00 +0100

    Add files via upload

--- Rechner.cs
+++ Rechner.cs
@@ -6,19 +6,37 @@
         {
             Random random = new Random();
 
-            int zahl1 = random.Next(1, 101);
-            int zahl2 = random.Next(1, 101);
-
-            bool Addition = random.Next(0, 2) == 0;
-
-            if (Addition)
+            for (int i = 0;i < 5;i++) 
             {
-                Console.Write($"Was ist {zahl1} + {zahl2}? ");
+                int zahl1 = random.Next(1, 101);
+                int zahl2 = random.Next(1, 101);
+
+                bool Addition = random.Next(0, 2) == 0;
+
+                if (Addition)
+                {
+                    Console.Write($"Was ist {zahl1} + {zahl2}? ");
+                }
+                else
+                {
+                    Console.Write($"Was ist {zahl1} - {zahl2}? ");
+                }
+
+                int benutzerAntwort = Convert.ToInt32(Console.ReadLine());
+
+                int korrekteAntwort = Addition ? (zahl1 + zahl2) : (zahl1 - zahl2);
+                if (benutzerAntwort == korrekteAntwort)
+                {
+                    Console.WriteLine("Richtig!");
+                }
+                else
+                {
+                    Console.WriteLine($"Falsch. Die richtige Antwort ist {korrekteAntwort}.");
+                }
+
             }
-            else
-            {
-                Console.Write($"Was ist {zahl1} - {zahl2}? ");
-            }
+
+            Console.ReadLine();
 
         }
     }
